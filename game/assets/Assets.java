640bf9d08dccf69661631215ad357b275aa0bfd4
Subder <T00924@g-able.com>
2022-12-19 01:41:32 +0700

    update logo

--- game/assets/Assets.java
+++ game/assets/Assets.java
@@ -4,7 +4,7 @@ public abstract class Assets {
     private Assets() {
     }
 
-    private static final String ASSETS_PATH = "assets/";
+    private static final String ASSETS_PATH = "onlygan/assets/";
 
     public static final String GAME_ICON = ASSETS_PATH + "onlygan.png";
 }
